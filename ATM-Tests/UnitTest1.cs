8b705cfe9cf046645d50641530478a07ade3acaa
Benjamin Taylor <staylor.ben@gmail.com>
2019-03-20 09:17:22 -0700

    Passed CanDeposit Test

--- ATM-Tests/UnitTest1.cs
+++ ATM-Tests/UnitTest1.cs
@@ -6,9 +6,11 @@ namespace ATM_Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanDeposit()
         {
-            
+            decimal balance = 0;
+            balance = ATM.Program.Deposit(balance, 500M);
+            Assert.Equal(500M, balance);
         }
     }
 }
--- ATM/Program.cs
+++ ATM/Program.cs
@@ -4,6 +4,10 @@ namespace ATM
 {
     public class Program
     {
+        public static Decimal Deposit(decimal balance, decimal amount)
+        {
+            return balance + amount;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
