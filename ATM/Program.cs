ac91ed2734c27ac7792a97af8324468ef9adc8c2
Benjamin Taylor <staylor.ben@gmail.com>
2019-03-20 10:10:30 -0700

    User can now deposit money

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -24,8 +24,30 @@ namespace ATM
             Console.WriteLine("1. View Balance");
             Console.WriteLine("2. Deposit Money");
             Console.WriteLine("3. Withdraw Money");
-            Console.WriteLine("4. Exit\n");
+            Console.WriteLine("4. Cancel\n");
             Console.Write("> ");
+        }
+
+        static bool AnotherTransaction()
+        {
+            string input;
+            do
+            {
+                Console.Write("Would you like another transaction? (y/n): ");
+                input = Console.ReadLine().ToLower();
+            } while (input != "y" && input != "n");
+            return input == "y";
+        }
+
+        static decimal HandleDeposit(decimal balance)
+        {
+            decimal amount;
+            do
+            {
+                Console.Write("Please enter an amount to deposit: $");
+            } while (!decimal.TryParse(Console.ReadLine(), out amount));
+            balance = Deposit(balance, amount);
+            return balance;
         }
 
         static void Main(string[] args)
@@ -40,10 +62,15 @@ namespace ATM
                 switch (input)
                 {
                     case "1":
+                        
+                        run = AnotherTransaction();
                         break;
                     case "2":
+                        balance = HandleDeposit(balance);
+                        run = AnotherTransaction();
                         break;
                     case "3":
+                        run = AnotherTransaction();
                         break;
                     case "4":
                         run = false;
