6d166aa0dd609812bb04dcefec8e2c89d787a648
Benjamin Taylor <staylor.ben@gmail.com>
2019-03-19 18:42:26 -0700

    Set up ATM & ATM-Tests projects

---
+++ ATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
